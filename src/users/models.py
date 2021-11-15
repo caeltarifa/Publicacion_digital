9778bf7df12783d0997d1e5c85c277a6aca17807
Carolina <cdavilasoliz@gmail.com>
2021-11-15 18:09:36 -0400

    PREVENT multipless session

---
+++ src/users/models.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.sessions.models import Session
+
+
+class UserSession(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    session = models.OneToOneField(Session, on_delete=models.CASCADE)
