444db8c24bc49cfa97ea72d48717f10fd1e07899
caeltarifa <caeltarifa@gmail.com>
2021-11-17 12:22:03 -0400

    JSON RESPONSE FOR DOCUMENTS

--- src/documents/urls.py
+++ src/documents/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import display_documents, add_documents, user_documents, delete_document
+from .views import display_documents, add_documents, user_documents, delete_document, get_document
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^my-documents/$', user_documents, name='user_doc'),
     url(r'^add/$', add_documents, name='add'),
     url(r'^delete/(?P<pk>\d+)/$', delete_document, name='delete'),
+    url(r'^get-document/(?P<pk>\d+)/$', get_document, name='get_document'),
 ]
