fb1194a2ffd4e6532e1daed81ec62789f2e75e83
caeltarifa <caeltarifa@gmail.com>
2021-11-23 08:27:21 -0400

    merge with the MAIN branch from dev

--- static_cdn/js/theme-scripts.js
+++ static_cdn/js/theme-scripts.js
@@ -11,7 +11,22 @@ $(function() {
       }
     }
   });
+
+  /*
+  $(document).on('click', 'a[href*=#]', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
+*/
 
   // hide #back-top first
   $("#back-top").hide();
